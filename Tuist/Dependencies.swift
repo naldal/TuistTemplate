693c112b7bb63daa130ce7e44c9e35be1664e0f4
naldal <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>
2023-06-12 23:01:29 +0900

    make tuist template generatable

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -11,7 +11,7 @@ let dependencies = Dependencies(
   carthage: CarthageDependencies([]),
   swiftPackageManager: SwiftPackageManagerDependencies([
     .remote(url: "https://github.com/Quick/Quick.git", requirement: .branch("master")),
-    .remote(url: "https://github.com/Quick/Nimble.git", requirement: .branch("master"))
+    .remote(url: "https://github.com/Quick/Nimble.git", requirement: .branch("main"))
   ], productTypes: [
     "Quick": .staticFramework,
     "Nimble": .staticFramework
